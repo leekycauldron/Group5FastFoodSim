d65763e771ae1e5eefb2a030b48c9736bb5174ba
Bryson <48774118+leekycauldron@users.noreply.github.com>
2024-04-03 17:08:32 -0400

    add title

--- WelcomeWorld.java
+++ WelcomeWorld.java
@@ -13,9 +13,19 @@ public class WelcomeWorld extends World
      * Constructor for objects of class WelcomeWorld.
      * 
      */
+    Label titleLabel = new Label("ChubMcFlub's", 40);
     public WelcomeWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        addObject(titleLabel,getWidth()/2,getHeight()/2);
     }
 }
